f45716f61ea5ba99645203cff22f398067d53f1d
manoharveeravalli24@gmail.com <manoharveeravalli24@gmail.com>
2020-07-19 09:20:38 +0530

    goal8

--- RDIP/Codes/Experiment.js
+++ RDIP/Codes/Experiment.js
@@ -38,20 +38,115 @@ function reform()
 displayingvalue= " "
 document.getElementById("play2").innerHTML =""
 document.getElementById("play3").innerHTML = displayingValue;
+document.getElementById("play4").innerHTML = ""
 for(var i=0;i <= 10;i++)
 {
 document.getElementById('button1'+i).style.display= "";
 }
 
 start=0;
+$("#hideSentence").hide()
 $("#correctsentence").hide()
 }
 
 
 
 
+var arrayvalue = ['John ate an apple before afternoon',
+'before afternoon John ate an apple',
+'John before afternoon ate an apple',
+'some students like to study in the night',
+'at night some students like to study'
+    ,'John and Mary went to church',
+    'Mary and John went to church',
+    'John went to church after eating', 
+    'after eating John went to church',
+    'John after eating went to church',
+    'did he go to market',
+    'he did go to market',
+    'the woman who called my sister sells cosmetics',
+    'the woman who sells cosmetics called my sister',
+    'my sister who sells cosmetics called the woman',
+    'my sister who called the woman sells cosmetics',
+    'John goes to the library and studies', 'John studies and goes to the library',
+    'John ate an apple so did she', 'she ate an apple so did John',
+    'the teacher returned the book after she noticed the error',
+    'the teacher noticed the error after she returned the book',
+    'after the teacher returned the book she noticed the error',
+    'after the teacher noticed the error she returned the book',
+    'she returned the book after the teacher noticed the error',
+    'she noticed the error after the teacher returned the book',
+    'after she returned the book the teacher noticed the error',    
+    'after she noticed the error the teacher returned the book',
+    'I told her that I bought a book yesterday',    
+    'I told her yesterday that I bought a book',
+    'yesterday I told her that I bought a book', 
+    'I bought a book that I told her yesterday',
+    'I bought a book yesterday that I told her',
+    'yesterday I bought a book that I told her',
+    'राम और श्याम बाजार गयें',
+    'राम और श्याम गयें बाजार',
+    'बाजार गयें राम और श्याम',
+    'गयें बाजार राम और श्याम',
+    'राम सोया और श्याम भी',
+    'श्याम सोया और राम भी',
+    'सोया श्याम और राम भी',
+    'सोया राम और श्याम भी',
+    'मैंने उसे बताया कि राम सो रहा है',
+    'मैंने उसे बताया कि सो रहा है राम',
+    'उसे मैंने बताया कि राम सो रहा है',
+    'उसे मैंने बताया कि सो रहा है राम',
+    'मैंने बताया उसे कि राम सो रहा है',
+    'मैंने बताया उसे कि सो रहा है राम',
+    'उसे बताया मैंने कि राम सो रहा है',
+    'उसे बताया मैंने कि सो रहा है राम',
+    'बताया मैंने उसे कि राम सो रहा है',
+    'बताया मैंने उसे कि सो रहा है राम',
+    'बताया उसे मैंने कि राम सो रहा है',
+    'बताया उसे मैंने कि सो रहा है राम',
+    'राम खाकर सोया',
+    'खाकर राम सोया',
+    'राम सोया खाकर',
+    'खाकर सोया राम',
+    'सोया राम खाकर',
+    'सोया खाकर राम',
+    'बिल्लियों को मारकर कुत्ता सो गया',
+    'मारकर बिल्लियों को कुत्ता सो गया',
+    'बिल्लियों को मारकर सो गया कुत्ता',
+    'मारकर बिल्लियों को सो गया कुत्ता',
+    'कुत्ता सो गया बिल्लियों को मारकर',
+    'कुत्ता सो गया मारकर बिल्लियों को',
+    'सो गया कुत्ता बिल्लियों को मारकर',
+    'सो गया कुत्ता मारकर बिल्लियों को',
+    'एक लाल किताब वहाँ है',
+    'एक लाल किताब है वहाँ',
+    'वहाँ है एक लाल किताब',
+    'है वहाँ एक लाल किताब',
+    'एक बड़ी सी किताब वहाँ है',
+    'एक बड़ी सी किताब है वहाँ',
+    'बड़ी सी एक किताब वहाँ है',
+    'बड़ी सी एक किताब है वहाँ',
+    'वहाँ है एक बड़ी सी किताब',
+    'वहाँ है बड़ी सी एक किताब',
+    'है वहाँ एक बड़ी सी किताब',
+    'है वहाँ बड़ी सी एक किताब'
+     ];
+function correct(){
+    var savev = displayingvalue.trim()
+    for(var i =0;i<arrayvalue.length;i++){
+        var temp1 = arrayvalue[i]
+        var localValue = temp1.localeCompare(savev)
+        if(localValue === 0){
+            document.getElementById("play4").innerHTML = "Right answer"
+            document.getElementById("play4").style.color = "00FF00"
+            return  document.getElementById("play4").innerHTML = "Right answer"
+            }
+            document.getElementById("play4").innerHTML = "Wrong answer"
+            document.getElementById("play4").style.color = "#FF0000"
+        }
+ }
 
-
+ 
 
 myfunction = function () {
 
@@ -117,6 +212,7 @@ myfunction = function () {
 var changeWords = Math.floor(Math.random() * earray.length);       
  document.getElementById("play2").innerHTML = ""
 	document.getElementById("play3").innerHTML = ""
+ document.getElementById("play4").innerHTML = ""
 	$("#hide").hide()
 	$("#correctsentence").hide()
 	
@@ -126,7 +222,7 @@ var changeWords = Math.floor(Math.random() * earray.length);
     start=0;
    
     var n = "";
-    for (i = 0; i <= k.length - 1; i++) {
+    for (var i = 0; i <= k.length - 1; i++) {
         val = k[i];
         m = "  <button style= 'font-size:15px ; padding:5px ; margin-right:5px ' id='btn1" + i + "' onclick='buttonDisplay(this.id,this.value)' value='" + val + "'>" + val + "</button>  ";
         n += m;
@@ -208,7 +304,9 @@ var changeWords = Math.floor(Math.random() * earray.length);
 
 	document.getElementById("play2").innerHTML = ""
 	document.getElementById("play3").innerHTML = ""
+	document.getElementById("play4").innerHTML = ""
 	$("#hide").hide()
+	$("#correctsentence").hide()
 
 
 
@@ -218,9 +316,9 @@ var changeWords = Math.floor(Math.random() * earray.length);
 	new=0;
 	start=0;
     var n = "";
-    for (i = 0; i <= k.length - 1; i++) {
+    for (var i = 0; i <= k.length - 1; i++) {
         val = k[i];
-        m = "  <button style= 'font-size:15px ; padding:4px ; margin-right:4px ' id='btn1" + i + "' onclick='buttonDisplay(this.id,this.value)' value='" + val + "'>" + val + "</button>  ";
+        m = "  <button style= 'font-size:20px ; padding:5px ; margin-right:5px ' id='btn1" + i + "' onclick='buttonDisplay(this.id,this.value)' value='" + val + "'>" + val + "</button>  ";
         n += m;
 	new++;
     }
@@ -232,12 +330,10 @@ var changeWords = Math.floor(Math.random() * earray.length);
  	document.getElementById("play2").innerHTML = ""
    	document.getElementById("play3").innerHTML = ""
         document.getElementById("value").innerHTML = ""
+
 	$("#hide").hide()
 	$("#correctsentence").hide()
         alert("select any language")
     }
 }
-
-
-
 
